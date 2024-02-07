85ead3ec576dd0d207d553e17bf7c3fd09bc5d1f
Harry <114489245+HyemIin@users.noreply.github.com>
2024-02-07 14:45:28 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 236 ms, Memory: 49208 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -2,11 +2,7 @@ import sys
 input = sys.stdin.readline
 
 n,m = map(int,input().split())
-site_name = dict()
-
-for i in range(n):
-    name,pw = map(str,input().split())
-    site_name[name] = pw
+site_name = dict(input().split() for _ in range(n))
 
 for i in range(m):
     site = input().rstrip()
