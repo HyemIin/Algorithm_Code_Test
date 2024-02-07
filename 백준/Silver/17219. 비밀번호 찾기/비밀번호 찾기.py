a3e1596aafa2406cc96dd09f950ed64e5fa94045
Harry <114489245+HyemIin@users.noreply.github.com>
2024-02-07 15:01:46 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 232 ms, Memory: 49040 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,10 +1,9 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline #입출력
 
-n,m = map(int,input().split())
-site_name = dict(input().split() for _ in range(n))
+n,m = map(int,input().split()) # , N,M 값 받기
+site_name = dict(input().split() for _ in range(n)) # HashTable의 일종인 dictionary를 활용하여  사이트/비밀번호 담기
 
 for i in range(m):
-    site = input().rstrip()
-    if site in site_name:
-        print(site_name[site])
+    site = input().rstrip() # 찾고자하는 사이트 입력 받기
+    print(site_name[site])
