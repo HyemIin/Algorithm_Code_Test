84fb2a66862335198cc3772982a8f4ea09ec72e0
Harry <114489245+HyemIin@users.noreply.github.com>
2024-02-12 16:08:05 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,22 +1,5 @@
-import sys
-input=sys.stdin.readline
 n = int(input())
-sequence = 1 #홀수면 SK, 짝수면 CY
-while 1:
-    if n%3 == 0: # n이 3의 배수라면
-        answer = n // 3
-        if answer%2 != 0 and sequence%2 != 0: # answer가 짝수라면 +  지금 차례 사람이 SK라면
-            print("SK")
-            break
-        elif answer%2 != 0 and sequence%2 == 0:
-            print("CY")
-            break
-        elif answer%2 == 0 and sequence%2 != 0:
-            print("CY")
-            break
-        elif answer%2 == 0 and sequence%2 == 0:
-            print("SK")
-            break
-    else:
-        n -= 1
-        sequence += 1
+if n % 2 == 0:
+    print('CY')
+else:
+    print('SK')
