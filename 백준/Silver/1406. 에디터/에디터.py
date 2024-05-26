2bde8e2218eb81720bf82ab2cfa77f5a31a12058
Harry <114489245+HyemIin@users.noreply.github.com>
2024-05-26 17:42:13 +0900

    [Silver II] Title: 에디터, Time: 304 ms, Memory: 37576 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,29 +1,23 @@
 import sys
-from collections import deque
-input=sys.stdin.readline
 
-s1 = list(input().strip())
-s2 = deque()
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
 
-n = int(input())
-data = [list(map(str,input().split())) for _ in range(n)]
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
 
-for command in data:
-    # L인 경우
-    if command[0] == "L":
-        if s1:
-            s2.insert(0,s1.pop())
-    # D인 경우
-    elif command[0] =="D":
-        if s2:
-            s1.append(s2.popleft())
-    # B인 경우
-    elif command[0] == "B":
-        if s1:
-            s1.pop()
+    elif command[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+
+    elif command[0] == 'B':
+        if st1:
+            st1.pop()
     else:
-        s1.append(command[1])
+        st1.append(command[1])
 
-
-s1.extend((s2))
-print("".join(s1))
+st1.extend(reversed(st2))
+print(''.join(st1))
